be962f2eb18ba834d09b8aa2ca6192e3a0fc7298
FrancyelZelling <zellingf@gmail.com>
2018-11-06 19:18:28 -0200

    Consulta com varios criterios

--- src/main/java/com/francyelzelling/workshopmongo/services/PostService.java
+++ src/main/java/com/francyelzelling/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.francyelzelling.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,17 +12,33 @@ import com.francyelzelling.workshopmongo.repository.PostRepository;
 import com.francyelzelling.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
+
 public class PostService {
 
 	@Autowired
+
 	private PostRepository repo;
 
 	public Post findById(String id) {
+
 		Optional<Post> obj = repo.findById(id);
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
-	
-	public List<Post> findByTitle(String text){
+
+	public List<Post> findByTitle(String text) {
+
 		return repo.searchTitle(text);
+
 	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+
+		return repo.fullSearch(text, minDate, maxDate);
+
+	}
+
 }
